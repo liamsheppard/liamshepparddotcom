1bbf89ac316871b94d9600ba78628c70e0ed5c77
Liam-Sheppard <lsheppard1995@gmail.com>
2016-01-15 18:21:42 +1000

    Restarted Site
    A new year means a new website design (well, that's my excuse anyway).

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,23 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var notify = require('gulp-notify');
+var directory = 'liamsheppard2016';
 
 gulp.task('default', function() {
-    gulp.watch('./liamsheppard2015/sass/**/*.scss',['sass']);
-    gulp.watch('./liamsheppard2015/css/**/*.css',['minify-css']);
+    gulp.watch('./' + directory + '/sass/**/*.scss',['sass']);
+    gulp.watch('./' + directory + '/css/**/*.css',['minify-css']);
 });
 
 gulp.task('sass', function () {
-    return gulp.src('./liamsheppard2015/sass/**/*.scss')
+    return gulp.src('./' + directory + '/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./liamsheppard2015/css'));
+        .pipe(gulp.dest('./' + directory + '/css'));
 });
 
-var cssMinifyLocation = ['./liamsheppard2015/css/**/*.css', '!./liamsheppard2015/css/**/*min.css'];
+var cssMinifyLocation = ['./' + directory + '/css/**/*.css', '!./' + directory + '/css/**/*min.css'];
 gulp.task('minify-css', function() {
     return gulp.src(cssMinifyLocation)
         .pipe(rename({ suffix: '.min' }))
-        .pipe(minifyCss({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('./liamsheppard2015/minified-css'));
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./' + directory + '/dist/css'));
 });
